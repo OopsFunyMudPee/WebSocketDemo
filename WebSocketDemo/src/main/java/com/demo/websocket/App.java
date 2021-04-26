bbd1a51ae8b6e1d0263ebb14ef5931d31e19f461
liukai <liukai@gtmap.cn>
2021-04-26 09:59:14 +0800

    init

--- WebSocketDemo/src/main/java/com/demo/websocket/App.java
+++ WebSocketDemo/src/main/java/com/demo/websocket/App.java
@@ -1,8 +1,11 @@
-package com.demo.websocket.websocketdemo;
+package com.demo.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 6k2
+ */
 @SpringBootApplication
 public class App {
 
